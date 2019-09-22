e277529eb95c12e3b792c2575439e9b54ae5cca4
Gwinbleind <borislav31@ya.ru>
2019-09-22 22:53:34 +0300

    Реализация на основе верстки магазина

--- lesson_1.js
+++ lesson_1.js
@@ -1,16 +1,31 @@
 const goods = [
-    { title: 'Компьютерная мышь', price: 400 },
-    { title: 'Жесткий диск SSD 1Tb', price: 10000 },
-    { title: 'Материнская плата', price: 4000 },
-    { title: 'Видео-карта', price: 15000 },
+    { title: 'Mango People T-shirt 1', price: 400, imgLink: 'img/Product_1.png'},
+    { title: 'Mango People T-shirt 2', price: 400, imgLink: 'img/Product_2.png'},
+    { title: 'Mango People T-shirt 3', price: 400, imgLink: 'img/Product_3.png'},
+    { title: 'Mango People T-shirt 4', price: 400, imgLink: 'img/Product_4.png'},
+    { title: 'Mango People T-shirt 5', price: 400, imgLink: 'img/Product_5.png'},
+    { title: 'Mango People T-shirt 6', price: 400, imgLink: 'img/Product_6.png'},
+    { title: 'Mango People T-shirt 7', price: 400, imgLink: 'img/Product_7.png'},
+    { title: 'Mango People T-shirt 8', price: 400, imgLink: 'img/Product_8.png'},
+    { title: 'Mango People T-shirt 9', price: 400, imgLink: 'img/Product_9.jpg'},
 ];
 const renderList = (items) => {
     const renderedGoods = items.map(renderItem).join('');
+    const catalogHeader = '<div class="products__box products__box_x3 div_flex"></div>';
     //Запятые стоят из-за того, что .map возвращает массив, разделенный запятыми. Чтобы их убрать, превратим массив в строку с помощью .join
-    document.querySelector('.catalog').innerHTML = renderedGoods;
+    document.querySelector('.catalog').insertAdjacentHTML("beforeend", catalogHeader);
+    document.querySelector('.products__box').insertAdjacentHTML("beforeend", renderedGoods);
 };
-const renderItem = ({title, price}) => {
-    return `<div class="item"><h3>${title}</h3><p>${price}</p></div>`;
+const renderItem = ({title, price, imgLink}) => {
+    return `
+<div class="product__element">
+    <a href="" class="product__content">
+        <img class="product__img" src="${imgLink}" alt="">
+        <div class="product__name">${title}</div>
+        <div class="product__price">$${price}</div>
+    </a>
+    <a href="#" class="product__add">Add to Cart</a>
+</div>`;
 };
 
 renderList(goods);
