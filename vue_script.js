6fa6f815115380a15220f64927a68cfe2d842c5b
Gwinbleind <borislav31@ya.ru>
2019-10-14 19:18:16 +0300

    First step

---
+++ vue_script.js
@@ -0,0 +1,16 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        //
+    },
+    methods: {
+        fetchCatalog(url) {
+            
+        },
+        handleSearchInput() {}
+    },
+    computed: {
+        //
+    }
+});
+console.log(app);
